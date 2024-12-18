2831a7fd3e00e513562ff954fffbfb1321785377
lchtao26 <52860060+lchtao26@users.noreply.github.com>
2024-12-18 11:00:42 +0800

    feat: remove less to css autocmd

--- .config/nvim/lua/custom.lua
+++
@@ -1,7 +0,0 @@
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd('BufWritePost', {
-  pattern = '*_lesson/**/*.less',
-  command = 'Dispatch! lessc "%" "%:p:h/%:t:r.css"',
-  group = 'Buf',
-})
